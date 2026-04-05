3abb07fc1e79118a7bb4dec294d0919763575a78
daniel <daniel.marques.bnb@gmail.com>
2026-04-04 21:08:53 -0300

    ThankYouForm

---
+++ src/components/Form/ThakYouForm.tsx
@@ -0,0 +1,11 @@
+
+export const ThakYou = () => {
+
+  return (
+    <section className="flex flex-col gap-3 items-center justify-center bg-White px-6 py-32 rounded-lg w-full">
+      <img className="mb-4" src="/assets/images/icon-thank-you.svg" alt="Thank you image" />
+      <h1>Thank you!</h1>
+      <p>Thanks for confirming your subscription! We hope you have fun using our plataform. If you ever need support, pleas feel free to email us at support@loremgaming.com.</p>
+    </section>
+  );
+};
